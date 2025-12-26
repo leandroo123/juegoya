3733c0c49d59f536baddc683738cd07389d47d02
Leandro <leandroerle32@gmail.com>
2025-12-26 11:49:03 -0300

    Update SEO metadata to JuegoYa branding

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
     "192.168.1.19:3000",
     "localhost:3000",
     "127.0.0.1:3000",
-    // opcional (a veces ayuda):
     "192.168.1.19",
     "localhost",
     "127.0.0.1",
